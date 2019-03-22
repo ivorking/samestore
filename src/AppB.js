5631c99aafb7a80092fcd33a152c067d933fadef
Ivor King <ivorking@gmail.com>
2019-03-22 01:21:34 +0000

    update

---
+++ src/AppB.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import TopBar from './components/TopNavBar';
+import RightColumn from './components/RightColumn';
+import LeftColumn from './components/LeftColumn';
+import VoteButton from './components/VoteButton'
+import './css/App.css';
+import './css/tachyons.css';
+
+class App extends Component {
+
+  state = {
+    leftcolumn: {},
+    rightcolumn: {},
+    voteButton: {}
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+        <header className="App-header">
+          <p>
+            Compare the store!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+        </header>
+        <LeftColumn />
+        <RightColumn />
+        <VoteButton />
+      </div>
+    );
+  }
+}
+
+export default App;
