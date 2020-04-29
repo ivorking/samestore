de8e503b7f8c5340d039e2099b59225e2c83d8c2
Ivor King <ivorking@gmail.com>
2020-04-29 08:24:38 +0100

    update

--- src/components/BottomLeft.js
+++ src/components/BottomLeft.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const BottomLeft = props => (
-    <div className = 'bottom-0 fixed w-50 h-25 inline-flex items-center justify-center bottom-0-l left-0'>
+    <div className = 'bottom-0 fixed w-25 h-25 inline-flex items-center justify-center bottom-0-l left-0'>
         BottomLeft section
     </div>
 )
