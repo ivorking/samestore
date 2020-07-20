e54dc094faf2dbde5943cef1f41e223bac85cd76
Ivor King <ivorking@gmail.com>
2020-07-20 10:06:17 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const Home = props => (
-    <div className = 'bottom-0 fixed w-50 h-25 inline-flex items-center justify-center bottom-0-l bg-blue'>
+    <div className = 'inline-flex ma0 bottom-0 bg-green fixed'>
         Home section
     </div>
 )
