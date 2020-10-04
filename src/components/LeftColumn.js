19732027c2dcd504ceed7b8238daf2670a5ce819
Ivor King <ivorking@gmail.com>
2020-10-04 21:58:55 +0100

    update

--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -4,63 +4,59 @@ import matchdata from '../data/matchdata.json';
 import '../css/tachyons.css';
 import 'tachyons-skins';
 
-var leftCountry = "Australia"
-var rightCountry = "UK"
+// define vars
+var leftCountry;
+var leftStore;
+var lstores;
+var lcountries;
 
 class LeftColumn extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      leftCountry: "",
-      leftStore: "",
-      lstores: {},
-      lcountries: {},
-      inputdataStores: []
-    }
+  componentWillMount = () => {
+    var inputdataCountries = Object.keys(matchdata);
+    lcountries = inputdataCountries.map(v => ({
+      label: v,
+      value: v
+    }));
   }
 
   handleChange = (event) => {
     this.setState({ [event.target.name ]: event.target.value })
   }
 
-  handleSelectionCountry = leftCountry => {
-    this.props.infoVar(leftCountry.value)
-  };
+  handleSelectionCountry = (leftCountry) => {
+    this.props.infoVar(leftCountry)
 
-  handleSelectionStore = leftStore => {
-    this.props.infoVar(leftStore.value)
-  };
+    // update left column store list
+    let templist = [];
+    let countryKeys = Object.keys(matchdata[leftCountry.value]);
+    let i;
+    for (i = 0 ; i < countryKeys.length ; i++) {
+      templist.push(Object.keys(matchdata[leftCountry.value][countryKeys[i]]))
+    }
+    let inputdataStores = templist.flat();
 
-  render() {
-
-    var inputdataCountries = Object.keys(matchdata);
-    this.lcountries = inputdataCountries.map(v => ({
+    lstores = inputdataStores.map(v => ({
       label: v,
       value: v
     }));
+  
+  };
 
-    if (leftCountry) {
-      let templist = [];
-      let countryKeys = Object.keys(matchdata[leftCountry]);
-      let i;
-      for (i = 0 ; i < countryKeys.length ; i++) {
-        templist.push(Object.keys(matchdata[leftCountry][countryKeys[i]]))
-      }
-      this.inputdataStores = templist.flat();
-  
-      this.lstores = this.inputdataStores.map(v => ({
-        label: v,
-        value: v
-      }));
-  
-    }
+  componentDidUpdate = () => {
+
+  }
+
+  handleSelectionStore = leftStore => {
+    this.props.infoVar(leftStore)
+  };
+
+  render() {
 
     return (
       <nav className = "LeftColumn pt4-ns black-80">
         <div className = "h2-ns">
           {this.props.infoVar}
-          {console.log(this.lcountries)}
           Enter the country and store you're finding an equivalent for...
         </div>
         <form className="measure center">
@@ -69,17 +65,17 @@ class LeftColumn extends React.Component {
               <label className="db fw6 lh-copy f6 tl">Country name:</label>
               {/* <input className="pa2 input-reset ba bg-transparent" type="text" name="country" value={this.state.lcountry} onChange={this.handleChange} /> */}
               <Select className = "tl pa2 input-reset bg-transparent" type="text" name="country" 
-                value={this.leftCountry}
+                value={leftCountry}
                 onChange={this.handleSelectionCountry}
-                options={this.lcountries}
+                options={lcountries}
               />
             </div>
             <div className="mt3">
               <label className="db fw6 lh-copy f6 tl">Store name:</label>
               <Select className = "t1 pa2 input-reset bg-transparent" type="text" name="store"
-                value={this.leftStore}
+                value={leftStore}
                 onChange={this.handleSelectionStore}
-                options={this.lstores}
+                options={lstores}
               />
             </div>
           </fieldset>
